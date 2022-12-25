3c5be3f7db32402d164ce81562aff5d9882e2fe0
joo-pe <joohyung1002@naver.com>
2022-12-25 17:07:35 +0900

    chapter11

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import NotificationList from './chapter_06/NotificationList';
 import Accommodate from './chapter_07/Accommodate';
 import ConfirmButton from './chapter_08/ComfirmButton';
 import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ConfirmButton/>
+    <SignUp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
